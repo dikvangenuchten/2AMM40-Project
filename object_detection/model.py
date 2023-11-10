8cf68728851c3e16845635420f6dac6edf3f0485
Dik van Genuchten <h.j.m.v.genuchten@student.tue.nl>
2023-11-10 14:24:39 +0000

    Add 1 to prototype index for easier matching

--- object_detection/model.py
+++ object_detection/model.py
@@ -254,7 +254,7 @@ class PIPSSD(SSD):
                     img.cpu(),
                     caption="Important prototypes found:"
                     + " ".join(
-                        f"[idx: {idx.cpu().detach().numpy()} val: {val.cpu().detach().numpy()}]"
+                        f"[idx: {idx.cpu().detach().numpy() + 1} val: {val.cpu().detach().numpy()}]"
                         for idx, val in zip(important_prototypes_idx.split(num_classes - 1), important_prototypes_val.split(num_classes - 1))
                     ),
                 )
